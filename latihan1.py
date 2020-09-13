aca992b18baa1b1ebfdf0ddaf8abb5c9518c258a
Achmad Hidayat <webmaster@achmad.web.id>
2020-09-13 13:17:23 +0700

    Initial Commit. Added index.html file contains Nothing :(

---
+++ latihan1.py
@@ -0,0 +1,36 @@
+#tipe data string
+nama = "Professor Achmad Hidayat"
+
+#tipe data integer
+umur = 18
+#tipe data float
+tinggi = 85,5
+
+#tipe data boolean
+benar = True
+salah = False
+
+#tipe data array atau list pada phyton
+nilai  = [1,2,3,4,5]
+
+#tipe data object atau dictionary pada python
+contoh = {
+    "key1" : "value1",
+    "key2" : "value2",
+    "key3" : "value3"
+}
+
+print(nama)
+print("=============")
+print(umur)
+print("=============")
+print(tinggi)
+print("=============")
+print(benar)
+print("=============")
+print(salah)
+print("=============")
+print (nilai[0])
+print("=============")
+print(contoh['key2'])
+print("=============")
