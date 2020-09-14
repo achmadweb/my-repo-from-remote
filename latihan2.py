58ccd75f682c089a020b95e2f20232b3843671e3
Achmad Hidayat <webmaster@achmad.web.id>
2020-09-14 14:40:36 +0700

    untuk latihan saja !

---
+++ latihan2.py
@@ -0,0 +1,10 @@
+nilai1 = 80
+nilai2 = 80
+
+if (nilai1 < nilai2):
+    print("Nilai1 kurang dari nilai2")
+elif(nilai1 == nilai2):
+    print("Nilai1 dan nilai2 adalah sama")
+else:
+    print("Nilai2 lebih besar dari nilai1")
+
